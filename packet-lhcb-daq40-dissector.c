39d8e1c1a98b4364e04b1a2c433a9668757e1175
Christina Quast <chrysh.ng+git@gmail.com>
2015-07-22 15:37:10 +0200

    diss: rename foo to mep

--- packet-lhcb-daq40-dissector.c
+++ packet-lhcb-daq40-dissector.c
@@ -1,29 +1,29 @@
 #include "config.h"
 #include <epan/packet.h>
-#define FOO_PORT 1234
+#define MEP_PORT 1234
 
-static int proto_foo = -1;
+static int proto_mep = -1;
 
-static void dissect_foo(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+static void dissect_mep(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
-    col_set_str(pinfo->cinfo, COL_PROTOCOL, "FOO");
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "MEP");
     /* Clear out stuff in the info column */
     col_clear(pinfo->cinfo,COL_INFO);
 }
 
-void proto_register_foo(void)
+void proto_register_mep(void)
 {
-    proto_foo = proto_register_protocol (
-        "FOO Protocol", /* name       */
-        "FOO",      /* short name */
-        "foo"       /* abbrev     */
+    proto_mep = proto_register_protocol (
+        "MEP Protocol", /* name       */
+        "MEP",      /* short name */
+        "mep"       /* abbrev     */
         );
 }
 
-void proto_reg_handoff_foo(void)
+void proto_reg_handoff_mep(void)
 {
-    static dissector_handle_t foo_handle;
+    static dissector_handle_t mep_handle;
 
-    foo_handle = create_dissector_handle(dissect_foo, proto_foo);
-    dissector_add_uint("udp.port", FOO_PORT, foo_handle);
+    mep_handle = create_dissector_handle(dissect_mep, proto_mep);
+    dissector_add_uint("udp.port", MEP_PORT, mep_handle);
 }
