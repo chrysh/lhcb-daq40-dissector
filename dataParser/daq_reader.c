1db48eff98152af700dd0283c636dd5f6f8f2591
Christina Quast <chrysh.ng+github@gmail.com>
2015-08-17 14:42:48 +0200

    DAQ reader: First draft of init, exit, read data functions
    The select system call is used to determine when the metadata device
    can deliver data.
    The data can then be read through DMA is is sent to wireshark.
    The function to send data to wireshark is noch fully implemented yet.
    
    The p40_daq_* functions are used in order to start and stop data
    generation and retrieve the pointer to the buffer, which contains
    the data in DMA memory region.
    
    For now, mock values for device numbers are used.

--- dataParser/daq_reader.c
+++ dataParser/daq_reader.c
@@ -2,47 +2,49 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-/* FIXME: What is the real meta device number? */
-#define META_DEV    0
-#define DATA_DEV    0
+#define DAQ_DEV    "/dev/random"
 #define BASIC_BSIZE 64  /* Basic size of data is 64 byte */
 
 /* Prototypes for PCIe40 driver */
+/** Metadata functions */
 extern int p40_ctl_open(int dev);
 extern void p40_ctl_close(int fd);
-
 extern int p40_ctl_start(int fd);
 extern int p40_ctl_stop(int fd);
 
+/** DAQ data functions */
 extern int  p40_daq_open(int dev, void **buffer);
 extern void p40_daq_close(int fd, void *buffer);
-
-
 extern int      p40_daq_set_read_off(int fd, uint32_t off);
 extern uint32_t p40_daq_get_read_off(int fd);
 extern uint32_t p40_daq_get_write_off(int fd);
 extern uint64_t p40_daq_get_buf_size(int fd);
 
-static char *databuf = NULL;
+extern int send_data_wireshark(void *waddr, uint32_t size);
+static int fd;
+
+static uint32_t *databuf = NULL;
 
 int dataParser_init()
 {
-    int fd_meta = p40_ctl_open(META_DEV);
-    if (fd_meta < 0) {
+    fd = open(DAQ_DEV, O_RDONLY);
+    int ret = p40_ctl_open(fd);
+    if (ret < 0) {
         perror("p40_ctl_open");
-        return fd_meta;
+        return ret;
     }
 
     /* p40_daq_open will store the pointer to the data in databuf */
-    int fd_data = p40_daq_open(DATA_DEV, (void **)&databuf);
-    if (fd_data < 0) {
+    ret = p40_daq_open(fd, (void **)&databuf);
+    if (ret < 0) {
         perror("p40_daq_open");
-        return fd_data;
+        return ret;
     }
 
-    p40_ctl_start(META_DEV);
-    p40_ctl_start(DATA_DEV);
+    p40_ctl_start(fd);
     return 0;
 }
 
@@ -50,53 +52,54 @@ void dataParser_exit()
 {
     int ret = 0;
 
-    p40_daq_close(DATA_DEV, databuf);
+    p40_daq_close(fd, databuf);
 
-    ret = p40_ctl_stop(META_DEV);
+    ret = p40_ctl_stop(fd);
     if (ret < 0) {
         /* FIXME: print error */
         return;
     }
-    p40_ctl_close(META_DEV);
+    p40_ctl_close(fd);
 }
 
 /* Reading data and sending it to wireshark should be one function,
  * with no need to pass buffer pointers between each other */
-int read_send_data()
+int read_send_data(int fd)
 {
-    uint32_t roff = p40_daq_get_read_off(DATA_DEV);
-    /* TODO: Read metadata, send it to wireshark */
+    uint32_t roff = p40_daq_get_read_off(fd);
+    uint32_t woff = p40_daq_get_write_off(fd);
+    /* Data can be read from databuf+roff now */
 
-    /* FIXME: Who updates the read offset? */
-    /* Data can be read from databuf+off now */
-
-    /* Send data to wireshark*/
-    send_data_wireshark(databuf+roff, BASIC_BSIZE);
+    int ret = send_data_wireshark((void *)databuf+roff, roff-woff);
+    if (ret != 0) {
+        printf("Error sending data to wireshark\n");
+        return ret;
+    }
+    p40_daq_set_read_off(fd, roff);
 }
 
 int check_for_data()
 {
-    fd_set meta_set;
+    fd_set daq_fd_set;
     struct timeval timeout;
 
     /* FIXME: How much timeout time makes sense? */
     timeout.tv_sec = 15;
     timeout.tv_usec = 0;
 
-    FD_ZERO (&meta_set);
-    FD_SET (META_DEV, &meta_set);
+    FD_ZERO (&daq_fd_set);
+    FD_SET (fd, &daq_fd_set);
 
-    /* FIXME: Do we have to wait for both, the metadata and data dev? */
     /* On success, ret contains number of file descr, that fired */
-    int ret = select( FD_SETSIZE, &meta_set, NULL, NULL, &timeout);
+    int ret = select( FD_SETSIZE, &daq_fd_set, NULL, NULL, &timeout);
 
     printf("ret: %d\n", ret);
     if (ret == 0) {
         printf("Timeout triggered\n");
     } else if (ret < 0) {
-        perror("Error on META_DEV I/O");
-    }else if (FD_ISSET(DATA_DEV, &meta_set)) {
-        read_send_data();
+        perror("Error on DAQ_DEV I/O");
+    }else if (FD_ISSET(fd, &daq_fd_set)) {
+        read_send_data(fd);
     } else {
         printf("Something went terribly wrong (more than one fd triggered?)\n");
     }
