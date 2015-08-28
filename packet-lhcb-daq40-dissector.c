f71fcd2dd2742b79233677f8dc820b18db41175f
Christina Quast <chrysh.ng+github@gmail.com>
2015-08-28 10:29:18 +0200

    defines for field sizes

--- packet-lhcb-daq40-dissector.c
+++ packet-lhcb-daq40-dissector.c
@@ -1,7 +1,11 @@
 #include "config.h"
 #include <epan/packet.h>
 #include <stdio.h>
-#define MEP_METADATA_PORT 1234
+#include <math.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#define MEP_METADATA_PORT 29899
 #define MEP_DATA_PORT 1235
 
 static int proto_mep = -1;
@@ -9,18 +13,26 @@ static int proto_meta = -1;
 static int proto_data = -1;
 static int proto_payload = -1;
 
-static int hf_meta_IDO = -1;
-static int hf_meta_baseaddr = -1;
-static int hf_meta_num_events = -1;
-static int hf_meta_evid_size = -1;
 static int hf_data_evid = -1;
 static int hf_data_type = -1;
 static int hf_data_size = -1;
 static int hf_data_payload = -1;
 //static int hf_data_payload_size = -1;
 
+static int hf_meta_seqn = -1;
+static int hf_meta_size = -1;
+static int hf_meta_num_frags = -1;
+static int hf_meta_evid = -1;
+static int hf_frag_bxid_gdl = -1;
+static int hf_frag_gdl = -1;
+static int hf_opt_bxid = -1;
+static int hf_opt_data_exists = -1;
+static int hf_opt_datalen = -1;
+static int hf_opt_data = -1;
+
 static gint ett_mep = -1;
 static gint ett_meta = -1;
+static gint ett_frag = -1;
 static gint ett_data = -1;
 static gint ett_payload = -1;
 
@@ -80,8 +92,13 @@ static void dissect_meta(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
     proto_tree *mep_tree = NULL;
     proto_item *meta_root = NULL;
     proto_tree *meta_tree = NULL;
+    proto_item *frag_item = NULL;
+    proto_tree *frag_tree = NULL;
+
     int offset = 0;
-    int num_events = 0;
+    guint16 num_frags = 0;
+    int data_offset = 0;
+    int i, j;
 
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "MEP2 metadata");
     printf("*** Dissect MEP metadata \n");
@@ -94,20 +111,83 @@ static void dissect_meta(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
         meta_root = proto_tree_add_item(mep_tree, proto_meta, tvb, 0, -1, ENC_NA);
         meta_tree = proto_item_add_subtree(meta_root, ett_meta);
 
-        proto_tree_add_item(meta_tree, hf_meta_IDO, tvb, offset, 8, ENC_BIG_ENDIAN);  // 64bit = 8 byte
+/***  MEP header ***/
+        proto_tree_add_item(meta_tree, hf_meta_seqn, tvb, offset, 4, ENC_BIG_ENDIAN);  // 32bit = 4 byte
+        guint32 seqn = tvb_get_bits32(tvb, offset*8, 32, FALSE);
+        printf("*** Seqn: %d\n", seqn);
+        offset += 4;
+        proto_tree_add_item(meta_tree, hf_meta_size, tvb, offset, 2, ENC_BIG_ENDIAN);
+        offset += 2;
+        proto_tree_add_item(meta_tree, hf_meta_num_frags, tvb, offset, 2, ENC_BIG_ENDIAN);
+        num_frags = tvb_get_bits16(tvb, offset*8, 16, FALSE);
+        col_append_fstr(pinfo->cinfo, COL_INFO, "Num. frags: %hu", num_frags);
+        offset += 2;
+        proto_tree_add_item(meta_tree, hf_meta_evid, tvb, offset, 8, ENC_BIG_ENDIAN);
         offset += 8;
-        proto_tree_add_item(meta_tree, hf_meta_baseaddr, tvb, offset, 8, ENC_BIG_ENDIAN);
-        offset += 8;
-        proto_tree_add_item(meta_tree, hf_meta_num_events, tvb, offset, 1, ENC_BIG_ENDIAN);
-        num_events = tvb_get_bits8(tvb, offset, 8);
-        printf("Number of events: %d\n", num_events);
-        col_append_fstr(pinfo->cinfo, COL_INFO, "Num. events: %d", num_events);
-        /* FIXME: should we construct an event ID to event size mapping here? */
-        offset += 1;
-        while (num_events > 0) {
-            proto_tree_add_item(meta_tree, hf_meta_evid_size, tvb, offset, 2, ENC_BIG_ENDIAN);
-            offset += 2;
-            num_events--;
+
+/***    MEP MSB prot ***/
+        for (i = 0; i < num_frags; i++) {
+
+            data_offset = offset*8;
+            int start_offset = offset;
+            printf("\n*** Frag num: %d\n", i);
+            uint32_t data = tvb_get_bits32(tvb, data_offset, 32, ENC_BIG_ENDIAN);
+            printf("data: %x\n", data);
+            uint32_t global_hdr = data;
+            uint16_t frag_bxid = global_hdr >> 20;
+            printf("FRG.bxid: %hu\n", frag_bxid);
+
+            uint32_t frag_gdl = global_hdr & 0xFFFFF;
+            printf("FRG.gdl: %u\n", frag_gdl);
+            
+            frag_item = proto_tree_add_text(meta_tree, tvb, offset, -1, "Frag #%d,FRG.bxid: %hu, FRG.gdl: %u", i, frag_bxid, frag_gdl);
+            frag_tree = proto_item_add_subtree(frag_item, ett_frag);
+
+#define GLOBAL_HEADER_BYTES 4
+            size_t event_bytes = GLOBAL_HEADER_BYTES + ceil(frag_gdl/8.0);
+            size_t padding = event_bytes % 8 ? (8 - (event_bytes % 8)) : 0;
+
+            proto_item *ti = proto_tree_add_bits_item(frag_tree, hf_frag_bxid_gdl, tvb, data_offset, 32, ENC_BIG_ENDIAN);
+            //proto_item_set_text(ti, "BXID: %hu: GDL: %u", global_hdr >> 20, global_hdr & 0xFFFFF);
+            //proto_tree_add_bits_item(frag_tree, hf_frag_gdl, tvb, data_offset, 20, ENC_BIG_ENDIAN);
+            data_offset += 32;
+            //proto_tree_add_item(frag_tree, hf_opt_data, tvb, data_offset, 2*8, ENC_BIG_ENDIAN); // 12 bits
+            /*** MEP amc40_lapp_dp_fv_msb_wide_bx12_mentor ***/
+#define NUM_LINKS   3
+            for (j = 0; j < NUM_LINKS; j++) {
+                printf("\nOPT (%d): ", j);
+                //proto_tree_add_item(frag_tree, hf_opt_bxid, tvb, offset, 2, ENC_BIG_ENDIAN); // 12 bits
+                printf("offset: %x ", offset);
+/* Sizes defined in bits*/
+#define BXID_SIZE           12  
+#define INFO_SIZE           1
+#define DATALEN_BITS_SIZE   7   
+                guint16 bxid = tvb_get_bits16(tvb, data_offset, BXID_SIZE, FALSE);
+                proto_tree_add_bits_item(frag_tree, hf_opt_bxid, tvb, data_offset, 12, FALSE);
+                //guint16 bxid = tvb_get_ntohs(tvb, data_offset);
+                printf("BXID: %x at offset %d, ", bxid, data_offset);
+                data_offset += BXID_SIZE;
+                proto_tree_add_bits_item(frag_tree, hf_opt_data_exists, tvb, data_offset, INFO_SIZE, FALSE); // 1 bit
+                char dataflag = tvb_get_bits8(tvb, data_offset, INFO_SIZE);
+                printf("Data exists: %x at offset %d, ", dataflag, data_offset);
+                data_offset += INFO_SIZE;
+                //proto_tree_add_item(frag_tree, hf_opt_datalen, tvb, offset, 4, ENC_BIG_ENDIAN); // 1 bit
+                    /* Data comes next */
+                    gint16 datalen = 4*tvb_get_bits16(tvb, data_offset, DATALEN_BITS_SIZE, FALSE);
+                    //proto_tree_add_bits_item(frag_tree, hf_opt_datalen, tvb, data_offset, 7, FALSE);
+                    proto_tree_add_uint_bits_format_value(frag_tree, hf_opt_datalen, tvb, data_offset, DATALEN_BITS_SIZE, 0, "%u", datalen);
+                    printf("Datalen: %hu at offset %d, ", datalen, data_offset);
+                    data_offset += DATALEN_BITS_SIZE;
+                if (dataflag == 0 && datalen > 0) {
+                    proto_tree_add_bits_item(frag_tree, hf_opt_data, tvb, data_offset, datalen, ENC_NA);
+                    printf("Data: %x at offset %d, ", data, data_offset);
+                    data_offset += datalen;
+                    col_clear(pinfo->cinfo,COL_INFO);
+                } /* FIXME: else? */
+            }
+            printf("Advancing by %d bytes\n", event_bytes + padding);
+            offset = start_offset + event_bytes + padding;
+            //offset += ceil(data_offset/8.0);
         }
     }
 }
@@ -118,40 +198,76 @@ void proto_register_mep(void)
     static gint *ett[] = {
         &ett_mep,
         &ett_meta,
+        &ett_frag,
         &ett_data,
         &ett_payload
     };
     static const value_string data_type_names[] = {
-        {0x1, "Type 1"},
-        {0x2, "Type 2"},
+        {0x1, "First Type"},
+        {0x2, "Second Type"},
         {0, NULL},
     };
 
     static hf_register_info hf_meta[] = {
-        { &hf_meta_IDO,
-            { "MEP2 meta: Base id", "mep.meta.id0",
-            FT_UINT64, BASE_DEC,
+        { &hf_meta_seqn,
+            { "MEP2: Sequence number", "mep.seq",
+            FT_UINT32, BASE_DEC,
             NULL, 0x0,
-            "id0", HFILL }
+            "seqn", HFILL }
         },
-        { &hf_meta_baseaddr,
-            { "MEP2 meta: Base address", "mep.meta.baseaddr",
-            FT_UINT64, BASE_HEX,
-            NULL, 0x0,
-            "base address", HFILL }
-        },
-        { &hf_meta_num_events,
-            { "MEP2 meta: Number of events", "mep.meta.numev",
-            FT_UINT8, BASE_HEX,
-            NULL, 0x0,
-            "number of events", HFILL }
-        },
-// FIXME: how to implement a variable number of fields?
-        { &hf_meta_evid_size,
-            { "MEP2 meta: Event ID0 size", "mep.meta.evid0size",
+        { &hf_meta_size,
+            { "MEP2: Size", "mep.size",
             FT_UINT16, BASE_DEC,
             NULL, 0x0,
-            "event size", HFILL }
+            "size", HFILL }
+        },
+        { &hf_meta_num_frags,
+            { "MEP2: Number of fragments", "mep.fragnum",
+            FT_UINT16, BASE_DEC,
+            NULL, 0x0,
+            "number of fragments", HFILL }
+        },
+        { &hf_meta_evid,
+            { "MEP2: Event id", "mep.evid",
+            FT_UINT64, BASE_DEC,
+            NULL, 0x0,
+            "event id", HFILL }
+        },
+        { &hf_frag_bxid_gdl,
+            { "MEP2 FRG: BXID, GDL", "mep.frg.bxid",
+            FT_UINT32, BASE_HEX,
+            NULL, 0,
+            "fragment bxid & gdl", HFILL }
+        },
+        { &hf_frag_gdl,
+            { "MEP2 FRG: gdl", "mep.frg.gdl",
+            FT_UINT32, BASE_DEC,
+            NULL, 0,
+            "fragment gdl", HFILL }
+        },
+        { &hf_opt_bxid,
+            { "MEP2 OPT: bxid", "mep.opt.bxid",
+            FT_UINT32, BASE_HEX,
+            NULL, 0,
+            "opt bxid", HFILL }
+        },
+        { &hf_opt_data_exists,
+            { "MEP2 OPT: Data exists flag", "mep.opt.data_flag",
+            FT_UINT8, BASE_HEX,
+            NULL, 0,
+            "opt dataflag", HFILL }
+        },
+        { &hf_opt_datalen,
+            { "MEP2 OPT: data length", "mep.opt.datalen",
+            FT_UINT16, BASE_DEC,
+            NULL, 0,
+            "opt datalen", HFILL }
+        },
+        { &hf_opt_data,
+            { "MEP2 OPT: data", "mep.opt.data",
+            FT_UINT8, BASE_HEX,
+            NULL, 0x0,
+            "opt data", HFILL }
         },
     };
 
